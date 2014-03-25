bdbb2607c24ba728ae4a6847319663f24efdcb8c
domenvajevec <domenvajevec@gmail.com>
2014-03-25 12:53:51 -0700

    add glyphicon functionality

--- app/controllers/dvds_controller.rb
+++ app/controllers/dvds_controller.rb
@@ -1,8 +1,13 @@
 class DvdsController < ApplicationController
 
+
   def index
     dvd_list = JSON.parse(Dvd.party)
-    @movies = dvd_list["movies"].sort_by {|movie| movie["ratings"]["critics_score"]}.reverse
+    @dvds = dvd_list["movies"].sort_by {|movie| movie["ratings"]["critics_score"]}.reverse
+  
+     #thumbs up and down variables
+    @rotten = "<span class='glyphicon glyphicon-thumbs-down'></span>"
+    @fresh =  "<span class='glyphicon glyphicon-thumbs-up'></span>"
   end
 
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,13 @@
 class MoviesController < ApplicationController
+  
   def index
     movie_list = JSON.parse(Movie.party)
     @movies = movie_list["movies"].sort_by {|movie| movie["ratings"]["critics_score"]}.reverse
+
+     #thumbs up and down variables
+    @rotten = "<span class='glyphicon glyphicon-thumbs-down'></span>"
+    @fresh =  "<span class='glyphicon glyphicon-thumbs-up'></span>"
   end
+  
+
 end
