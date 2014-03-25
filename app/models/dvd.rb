e134273e64c8a52bd9f09d6f1eeb6a3cf31b4a23
domenvajevec <domenvajevec@gmail.com>
2014-03-25 12:17:14 -0700

    rename application.css to scss, add HTTParty functionality to movie and dvd model

--- app/models/dvd.rb
+++ app/models/dvd.rb
@@ -1,2 +1,7 @@
 class Dvd < ActiveRecord::Base
+  def self.party
+    search_url = "http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/new_releases.json?page_limit=16&page=1&country=us&apikey=cb2px8nd3bugrpdpd2h2quh7"
+    response = HTTParty.get search_url
+    #binding.pry
+  end
 end
