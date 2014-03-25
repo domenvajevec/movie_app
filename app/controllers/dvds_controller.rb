280594deaeef68744eba41303a45e2c44908fa0b
domenvajevec <domenvajevec@gmail.com>
2014-03-25 12:20:53 -0700

    add movie index page

--- app/controllers/dvds_controller.rb
+++ app/controllers/dvds_controller.rb
@@ -1,4 +1,8 @@
 class DvdsController < ApplicationController
+
   def index
+    dvd_list = JSON.parse(Dvd.party)
+    @movies = dvd_list["movies"].sort_by {|movie| movie["ratings"]["critics_score"]}.reverse
   end
+
 end
