363a488b6d7e7ec52d121340310606c63208e8a4
domenvajevec <domenvajevec@gmail.com>
2014-03-25 12:25:06 -0700

    disable turbolinks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #bootstrap glyphicon variables - thumbs up and down
+  @rotten = "<span class='glyphicon glyphicon-thumbs-down'></span>"
+  @fresh =  "<span class='glyphicon glyphicon-thumbs-up'></span>"
 end
