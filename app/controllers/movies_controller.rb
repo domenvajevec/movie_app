831eabb2ded6eed6c69d26fe6a4c929b19c571b3
domenvajevec <domenvajevec@gmail.com>
2014-03-27 12:03:57 -0700

    add jquery animation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,8 +5,5 @@ class MoviesController < ApplicationController
     movie_list = JSON.parse(Movie.party)
     @movies = movie_list["movies"].sort_by {|movie| movie["ratings"]["critics_score"]}.reverse
   end
- 
-  def show
-    movie_number = params[:id]
-  end
+
 end
