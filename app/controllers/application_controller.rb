ccc77ddcd8469cfc1dd34628ebe70063be75492b
domenvajevec <domenvajevec@gmail.com>
2014-03-27 10:34:12 -0700

    tweak responsive navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   def set_initial_id
-    @id = rand(5)
+    @id = rand(6)
     @rotten = "<span class='glyphicon glyphicon-thumbs-down'></span>"
     @fresh =  "<span class='glyphicon glyphicon-thumbs-up'></span>"
   end
-
+  
   helper_method :set_initial_id
+ 
 end
